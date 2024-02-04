eb99e926fdaf03f59593b7755e8c05014bb38bb6
Sabita Nepal <86512740+nepal-sabita@users.noreply.github.com>
2024-02-04 09:20:30 +0545

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
-#!/bin/sh
+#this script is to run all the packages and dependencies for the app
+#!/bin/bash
 
-#Update package list and install required dependencies
-echo
-          echo 4. Which build installed?
-          gradle --version
-          if [["$(ruby --version)"!= *"2.7.7"*]];
+# Update package list and install required dependencies
+sudo apt update
+sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
+
+# Installing rbenv
+curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+# Ruby installation
+if [["$(ruby --version)"!= *"2.7.7"*]];
           then
           echo "Ruby version is not found. Installing...."
           rbenv install 2.7.7
@@ -13,54 +21,35 @@ echo
           echo "ruby version 2.7.7 is already installed"
           fi
           ruby --version
-          if ! gem list -i "^rails$" > /dev/null 2>&1; then
-            echo "Rails not found. Installing..."
-            export GEM_HOME=$HOME/gems
-            export PATH=$GEM_HOME/bin:$PATH
-            gem install rails
-          else
-            echo "Rails is already installed."
-          fi
-          # Verify Rails installation
-          rails --version
-          if ! command -v flutter > /dev/null 2>&1;
+echo 'export PATH="$HOME/.gem/ruby/2.7.0/bin:$PATH"' >> ~/.bashrc
+source ~/.bashrc
+
+# Installation of Bundler and Rails
+echo "gem: --no-document" > ~/.gemrc
+gem env home
+gem install bundler --user-install
+if [["$(rails --version)"!= *"7.0.8"*]];
           then
-          echo "flutter not found. Installing ...."
-          git clone https://github.com/flutter/flutter.git -b stable --depth 1
-          export PATH="$PATH:`pwd`/flutter/bin"
-          flutter --version
-          flutter precache
+          echo "Rails version is not found. Installing...."
+          gem install rails -v 7.0.8
           else
-          echo "flutter is already installed"
+          echo "rails version 7.0.8 is already installed"
           fi
-          
-          flutter --version
-          dart --version
-          if ! command -v dart > /dev/null 2>&1;
-          then
-          echo "dart not found. Installing ..."
-          mkdir -p $HOME/dart
-            curl -fsSL https://dart.dev/get/dart.zip -o dart.zip
-            unzip dart.zip -d $HOME/dart
-            export PATH="$PATH:$HOME/dart/dart-sdk/bin"
-            else
-            echo "dart is already installed"
-            fi
-            dart --version
-          echo 5. Where is Android SDK Root
-          echo $ANDROID_SDK_ROOT
-          echo
-          echo 6. What is the workspace Location
-          echo $RUNNER_WORKSPACE
-          echo
-          echo 7. Who is running the script
-          whoami
-          echo 8. How is the disc laid out
-          df
-          echo 9. What environment variables are available
-          env
-          
-          
-          
-          
-          
+
+rails -v
+
+# Installation of PostgreSQL and libpq-dev
+if ! dpkg -l | grep -q postgresql; then
+    sudo apt install -y postgresql postgresql-contrib libpq-dev
+    else
+    echo "postgresql already install"
+fi
+psql --version
+# sudo apt install postgresql postgresql-contrib libpq-dev
+
+# Creating a PostgreSQL user and database
+sudo -u postgres createuser --no-superuser --no-createrole --createdb $USER
+sudo -u postgres createdb --owner=$USER DummyDb
+sudo -u postgres psql -d DummyDb -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO $USER;"
+sudo -u postgres psql -d DummyDb -c "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO $USER;"
+sudo -u postgres psql -d DummyDb -c "ALTER USER $USER WITH CREATEDB;"
