002efb5d8679160fc848ce14a870dd6aea0a3ca0
Sabita Nepal <86512740+nepal-sabita@users.noreply.github.com>
2024-02-04 09:29:07 +0545

    add conditin for rbenv setup.sh

--- setup.sh
+++ setup.sh
@@ -6,10 +6,16 @@ sudo apt update
 sudo apt install git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev
 
 # Installing rbenv
-curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+if ! command -v rbenv &> /dev/null; 
+          then
+          echo "Rbenv not found. Installing...."
+          curl -fsSL https://github.com/rbenv/rbenv-installer/raw/HEAD/bin/rbenv-installer | bash
+           echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+    echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+    source ~/.bashrc
+          else
+          echo "rbenv is already installed"
+          fi
 
 # Ruby installation
 if [["$(ruby --version)"!= *"2.7.7"*]];
